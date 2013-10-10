5d53b3c14e6b69ccce3691b2f77f0b32e3a65837
BoonsNaibot <deguzouwa@yahoo.com>
2013-10-09 20:16:38 -0500

    Update dndlistview.py

--- dndlistview.py
+++ dndlistview.py
@@ -8,16 +8,52 @@ class DragNDropListView(ListView):
 	"""This is an altered ListView to allow Drag 'N' Drop functionality."""
     placeholder = ObjectProperty(None, allownone=True)
     
+    def __init__(self, **kwargs):
+        self.register_event_type("on_drag_start")
+        self.register_event_type("on_drag_finish")
+        self.register_event_type("on_pos_change")
+        self.register_event_type("on_motion_over")
+        super(DragNDropListView, self).__init__(**kwargs)
+
+    def on_drag_start(self, widget):
+        children = self.container.children
+        adapter = self.adapter
+        for child in children:
+            if ((type(child) is not Widget) and child.is_selected):
+                adapter.deselect_item_view(child)
+                return
+
+    def on_drag_finish(self, widget):
+        pass
+
+    def on_pos_change(self, widget):
+        placeholder = self.placeholder
+        if not placeholder:
+            pass
+        
+        children = self.container.children
+        p_ix = children.index(placeholder)
+
+        for child in children:
+            if (widget.collide_widget(child) and (child is not placeholder)):
+                c_ix = children.index(child)
+
+                if ((widget.center_y <= child.top) and (widget.center_y <= placeholder.y)) or ((widget.center_y >= child.y) and (widget.center_y >= placeholder.top)):
+                    children.insert(c_ix, children.pop(p_ix))
+
+                #maybe scroll here
+                return
+                
     def deparent(self, widget):
         container = self.container
         placeholder = self.placeholder = Placeholder(size=widget.size,
                                                      size_hint_y=None,
                                                      index=widget.index,
+                                                     ix=widget.ix,
                                                      opacity=0.0)
 
-        ix = container.children.index(widget)
+        container.add_widget(placeholder, container.children.index(widget))
         container.remove_widget(widget)
-        container.add_widget(placeholder, ix)
         widget.size_hint_x = None
         container.get_root_window().add_widget(widget)
         return
@@ -25,14 +61,17 @@ class DragNDropListView(ListView):
     def reparent(self, widget):
         placeholder = self.placeholder
         if not placeholder:
+            self.dispatch('on_motion_over', widget)
             return
 
         container = self.container
         if placeholder.collide_widget(widget):
-            ix = container.children.index(placeholder)
             container.remove_widget(placeholder)
             container.get_root_window().remove_widget(widget)
-            container.add_widget(widget, ix)
+            container.add_widget(widget, container.children.index(placeholder))
             widget.size_hint_x = 1.
-            widget.index = placeholder.index
+            widget.ix = placeholder.ix
             self.placeholder = None
+
+    def on_motion_over(self, widget):
+        pass
